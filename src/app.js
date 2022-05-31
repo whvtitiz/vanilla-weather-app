c801047f7b09ea044bab6670f583fec43de6e7b6
whvtitiz <whitttwiles@gmail.com>
2022-05-30 18:47:30 -0700

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,17 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].dexcription);
 }
 
-let apiKey = "2e41d3b7f40986b8c574b9b6102eaef6";
-let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "2e41d3b7f40986b8c574b9b6102eaef6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
